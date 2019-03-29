58c92a1d84af7fde0969351302ef449a576b62d6
IvanCaldwell <grumpy.caldwell@gmail.com>
2019-03-29 10:45:49 -0700

    Adding new view controllers for todo features

---
+++ TeamHome/TeamHome/View Controllers/TodoListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TodoListViewController.swift
+//  TeamHome
+//
+//  Created by Ivan Caldwell on 3/28/19.
+//  Copyright © 2019 Lambda School under the MIT license. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
